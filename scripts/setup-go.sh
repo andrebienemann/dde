12bf25855806043a9634417c4401cb6c6146fde6
andrebienemann <andre.bienemann@gmail.com>
2024-06-29 11:32:28 +1000

    Modify Go setup

--- scripts/setup-go.sh
+++ scripts/setup-go.sh
@@ -3,16 +3,16 @@
 arch=$(dpkg --print-architecture)
 version="$(curl https://go.dev/VERSION\?m\=text | grep go).linux-$arch.tar.gz"
 
-wget -q "https://go.dev/dl/$version" 
+wget -q "https://go.dev/dl/$version" -O /tmp/$version
 
-tar -C /usr/local -xzf $version && rm $version
+tar -C /usr/local -xzf /tmp/$version && rm /tmp/$version
 ln -sf /usr/local/go/bin/go /usr/local/bin/go
 
 export GOPATH=/usr/local/etc/go
 
-echo -e "\n export GOPATH=$GOPATH" >> ~/.bashrc
-echo -e "\n export PATH=\$PATH:\$GOPATH/bin" >> ~/.bashrc
-echo -e "\n export GOPATH=$GOPATH" >> ~/.zshrc
-echo -e "\n export PATH=\$PATH:\$GOPATH/bin" >> ~/.zshrc
+echo -e "\nexport GOPATH=$GOPATH" >> ~/.bashrc
+echo -e "\nexport PATH=\$PATH:\$GOPATH/bin" >> ~/.bashrc
+echo -e "\nexport GOPATH=$GOPATH" >> ~/.zshrc
+echo -e "\nexport PATH=\$PATH:\$GOPATH/bin" >> ~/.zshrc
 
 mkdir -p $GOPATH
