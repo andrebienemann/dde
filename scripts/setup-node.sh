a37c4d5eec2a3df4643b5d2b62f12cdb4eb5ff9e
andrebienemann <andre.bienemann@gmail.com>
2024-06-29 10:11:21 +1000

    Upgrade Node to version 21

--- scripts/setup-node.sh
+++ scripts/setup-node.sh
@@ -2,8 +2,8 @@
 
 mkdir -p /etc/apt/keyrings
 
-curl -fsSL https://deb.nodesource.com/gpgkey/nodesource-repo.gpg.key | gpg --dearmor -o /etc/apt/keyrings/nodesource.gpg
+curl -fsSL https://deb.nodesource.com/gpgkey/nodesource-repo.gpg.key | gpg --dearmor --yes -o /etc/apt/keyrings/nodesource.gpg
 
-echo "deb [signed-by=/etc/apt/keyrings/nodesource.gpg] https://deb.nodesource.com/node_18.x nodistro main" | tee /etc/apt/sources.list.d/nodesource.list
+echo "deb [signed-by=/etc/apt/keyrings/nodesource.gpg] https://deb.nodesource.com/node_21.x nodistro main" | tee /etc/apt/sources.list.d/nodesource.list
 
-apt update && apt install -yqq nodejs npm
+apt update && apt install -yqq nodejs
