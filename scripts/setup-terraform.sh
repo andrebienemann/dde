4208c11936d9005af44bf8e5a414f37b2e3ddf28
andrebienemann <andre.bienemann@gmail.com>
2024-07-06 11:34:44 +1000

    Modify Terraform setup

--- scripts/setup-terraform.sh
+++ scripts/setup-terraform.sh
@@ -4,5 +4,6 @@ arch=$(dpkg --print-architecture)
 index=$(curl -sL https://releases.hashicorp.com/terraform/index.json)
 url="$(echo $index | jq -r ".versions[].builds[].url" | egrep -v "rc|beta|alpha" | egrep "linux.*$arch" | tail -1)"
 
-wget -q $url && unzip -oq ${url##*/} -d /usr/local/terraform && rm ${url##*/}
+wget -q $url -O /tmp/terraform
+unzip -oq /tmp/terraform -d /usr/local/terraform && rm /tmp/terraform
 ln -sf /usr/local/terraform/terraform /usr/local/bin/terraform
