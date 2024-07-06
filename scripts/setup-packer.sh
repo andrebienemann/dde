001a1e698b9f66ae7e450a1d71c2fb5809a077d0
andrebienemann <andre.bienemann@gmail.com>
2024-07-06 11:23:22 +1000

    Modify Packer setup

--- scripts/setup-packer.sh
+++ scripts/setup-packer.sh
@@ -4,5 +4,6 @@ arch=$(dpkg --print-architecture)
 index=$(curl -sL https://releases.hashicorp.com/packer/index.json)
 url="$(echo $index | jq -r ".versions[].builds[].url" | egrep -v "rc|beta|alpha" | egrep "linux.*$arch" | tail -1)"
 
-wget -q $url && unzip -oq ${url##*/} -d /usr/local/packer && rm ${url##*/}
+wget -q $url -O /tmp/packer
+unzip -oq /tmp/packer -d /usr/local/packer && rm /tmp/packer
 ln -sf /usr/local/packer/packer /usr/local/bin/packer
